e41f1694eeb0e3b7e79631afaf2327c64570f046
Tomas Balpataki <tomas.balpataki@gmail.com>
2023-06-25 23:34:26 +0200

    Solution for Advent of code 2015/01

---
+++ adventofcode/year-2015/day-01/solution.py
@@ -0,0 +1,20 @@
+from adventofcode.utils import solve
+
+
+def solve_part_i(inp, debug):
+    return inp.count("(") - inp.count(")")
+
+
+def solve_part_ii(inp, debug):
+    cnt = 0
+    floor = 0
+    while floor != -1:
+        floor += 1 * (1 if inp[cnt:cnt+1] == "(" else -1)
+        cnt += 1
+        if debug:
+            print(f"   Step: {cnt}, order: {inp[cnt:cnt + 1]}, new floor: {floor}")
+    return cnt
+
+
+solve(part=None, solver=solve_part_i, test_prefix="test_01_", test_expected_results=[0, 0, 3, 3, 3, -1, -1, -3, -3])
+solve(part=None, solver=solve_part_ii, test_prefix="test_02_", test_expected_results=[1, 5])
