f117d88d78c8b28f8ba11d5eecba80fcf38ef9e4
Tomas Balpataki <tomas.balpataki@gmail.com>
2023-06-27 19:40:49 +0200

    Solution for Advent of code 2015/04

--- adventofcode/year_2015/day_03/problem.md
+++ adventofcode/year_2015/day_03/problem.md
@@ -1,5 +1,7 @@
 # Day 3: Perfectly Spherical Houses in a Vacuum
 
+https://adventofcode.com/2015/day/3
+
 Santa is delivering presents to an infinite two-dimensional grid of houses.
 
 He begins by delivering a present to the house at his starting location, and then an elf at the North Pole calls him via radio and tells him where to move next. Moves are always exactly one house to the north (^), south (v), east (>), or west (<). After each move, he delivers another present to the house at his new location.
---
+++ adventofcode/year_2015/day_04/solution.py
@@ -0,0 +1,23 @@
+from adventofcode.utils import solve
+import hashlib
+
+
+def solve_problem(inp: str, expected_prefix: str, debug: bool = False):
+    num = 1
+    while not hashlib.md5((inp + str(num)).encode()).hexdigest().startswith(expected_prefix):
+        if debug and num % 1000 == 0:
+            print(f"Number: {num}, md5: {hashlib.md5((inp + str(num)).encode()).hexdigest()}")
+        num += 1
+    return num
+
+
+def solve_part_i(inp: str, debug: bool = False):
+    return solve_problem(inp, "00000", debug)
+
+
+def solve_part_ii(inp: str, debug: bool = False):
+    return solve_problem(inp, "000000", debug)
+
+
+solve(part=None, solver=solve_part_i, test_prefix="test_", test_expected_results=[609043, 1048970], debug=False)
+solve(part=None, solver=solve_part_ii, test_prefix=None, test_expected_results=None, debug=True)
