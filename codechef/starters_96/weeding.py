b454ea6e7f944c7f13faca33926bb46d7f068fad
Tomas Balpataki <tomas.balpataki@gmail.com>
2023-06-28 23:22:55 +0200

    Start working on the problem 'Weeding'

---
+++ codechef/starters_96/weeding.py
@@ -0,0 +1,25 @@
+"""
+Sample
+Input
+3
+3 10 1
+4 7 10
+3 10 2
+4 7 10
+4 10 5
+1 2 3 4
+Output
+Yes
+No
+Yes
+"""
+
+"""
+    n - number of weeds
+    m - total number of days
+    k - number of sprays to kill a weed
+    a - list of days on which new weeds show up
+"""
+for tc in range(int(input())):
+    n, m, k = map(int, input().split())
+    a = list(map(int, input().split()))
