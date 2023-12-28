7501816ce741e70ba627a3a1bd6bf75d897ba3cd
Tomas Balpataki <tomas.balpataki@gmail.com>
2023-12-28 19:50:41 +0100

    Some refactor

--- adventofcode/year_2015/day_05/solution.py
+++ adventofcode/year_2015/day_05/solution.py
@@ -3,7 +3,7 @@ from adventofcode.utils import solve
 import re
 
 
-def has_duplicated_pair(word: str, debug: bool = False):
+def has_duplicated_pair_check(word: str, debug: bool = False):
     for i in range(len(word) - 3):
         if word[i:i+2] in word[i+2:]:
             if debug:
@@ -12,7 +12,7 @@ def has_duplicated_pair(word: str, debug: bool = False):
     return False
 
 
-def has_shifted_duplicate(word: str):
+def has_shifted_duplicate_check(word: str):
     for i in range(len(word) - 2):
         if word[i+2] == word[i]:
             return True
@@ -22,15 +22,15 @@ def has_shifted_duplicate(word: str):
 def solve_part_i(inp: str, debug: bool = False):
     nice_words_cnt = 0
     for word in inp.splitlines():
-        vowels = len(re.findall("[aeiou]", word)) > 2
-        duplicate = len(re.findall(r"((\S)\2{1,})", word)) > 0
-        blacklisted = len(re.findall("(ab)|(cd)|(pq)|(xy)", word)) > 0
+        has_vowels = len(re.findall("[aeiou]", word)) > 2
+        has_duplicated_element = len(re.findall(r"((\S)\2{1,})", word)) > 0
+        has_blacklisted_elements = len(re.findall("(ab)|(cd)|(pq)|(xy)", word)) > 0
         if debug:
             print(f"\tWord: {word}, "
-                  f"has at least three vowels: {vowels}, "
-                  f"has duplicated letter: {duplicate}, "
-                  f"blacklisted element: {blacklisted}")
-        if vowels and duplicate and not blacklisted:
+                  f"has at least three vowels: {has_vowels}, "
+                  f"has duplicated letter: {has_duplicated_element}, "
+                  f"blacklisted element: {has_blacklisted_elements}")
+        if has_vowels and has_duplicated_element and not has_blacklisted_elements:
             nice_words_cnt += 1
     return nice_words_cnt
 
@@ -38,13 +38,13 @@ def solve_part_i(inp: str, debug: bool = False):
 def solve_part_ii(inp: str, debug: bool = False):
     nice_words_cnt = 0
     for word in inp.splitlines():
-        duplicated_pair = has_duplicated_pair(word, debug)
-        shifted_duplicate = has_shifted_duplicate(word)
+        has_duplicated_pair = has_duplicated_pair_check(word, debug)
+        has_shifted_duplicate = has_shifted_duplicate_check(word)
         if debug:
             print(f"\tWord: {word}, "
-                  f"has duplicated pair: {duplicated_pair}, "
-                  f"has shifted duplicate: {shifted_duplicate}")
-        if duplicated_pair and shifted_duplicate:
+                  f"has duplicated pair: {has_duplicated_pair}, "
+                  f"has shifted duplicate: {has_shifted_duplicate}")
+        if has_duplicated_pair and has_shifted_duplicate:
             nice_words_cnt += 1
     return nice_words_cnt
 
